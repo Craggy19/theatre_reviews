a8e7517def3da6803ddbe42127c5a7f44e5a713f
Craggy19 <cragg2@hotmail.co.uk>
2021-02-11 15:40:10 +0000

    Added delete confirm modal

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('select').formSelect();
+    $(".tooltipped").tooltip();
+$(document).ready(function(){
+    $('.modal').modal();
+  });
   });
